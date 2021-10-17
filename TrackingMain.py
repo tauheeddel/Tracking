c65214c4ca724688da271499e60424a4dd589178
Tauheed Khan Mohd <tauheed.del@gmail.com>
2021-10-17 15:55:31 -0500

    added

--- TrackingMain.py
+++ TrackingMain.py
@@ -7,11 +7,26 @@ success, img = cap.read()
 bbox = cv2.selectROI("Tracking", img, False)
 tracker.init(img, bbox)
 
+
+def drawBox():
+    x,y,w,h = int(bbox[0],bbox[1],bbox[2],bbox[3])
+    cv2.rectangle(img, (x,y), ((x+w), (y+h)), (255,0,255), 3,1)
+    cv2.putText(img, "Tracking", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+
 while True:
     timer = cv2.getTickCount()
     success, img = cap.read()
 
     success, bbox = tracker.update(img)
+    print(bbox)
+
+    print(type(bbox))
+
+    if success:
+        drawBox()
+    else:
+        cv2.putText(img, "Lost", (75,75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,0), 2)
 
     fps = cv2.getTickFrequency()/(cv2.getTickCount() -timer)
     cv2.putText(img, str(int(fps)), (75,50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,0), 2)
