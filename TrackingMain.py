634449e9bcdb1f56bdb14f931a40e97b1874149e
Tauheed Khan Mohd <tauheed.del@gmail.com>
2021-10-17 16:18:17 -0500

    added

--- TrackingMain.py
+++ TrackingMain.py
@@ -3,6 +3,7 @@ import cv2
 cap = cv2.VideoCapture(0)
 
 tracker = cv2.TrackerMOSSE_create()
+#tracker =cv2.TrackerCSRT_create()
 success, img = cap.read()
 bbox = cv2.selectROI("Tracking", img, False)
 tracker.init(img, bbox)
@@ -26,10 +27,10 @@ while True:
     if success:
         drawBox()
     else:
-        cv2.putText(img, "Lost", (75,75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,0), 2)
+        cv2.putText(img, "Lost", (75,75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
 
     fps = cv2.getTickFrequency()/(cv2.getTickCount() -timer)
-    cv2.putText(img, str(int(fps)), (75,50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,255,0), 2)
+    cv2.putText(img, str(int(fps)), (75,50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255,0,0), 2)
     cv2.imshow("Tracking", img)
     if cv2.waitKey(1) & 0xff ==('q'):
         break
