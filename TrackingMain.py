3223174be81d3e09f521facd71259020d8004199
Tauheed Khan Mohd <tauheed.del@gmail.com>
2021-10-17 16:00:19 -0500

    added

--- TrackingMain.py
+++ TrackingMain.py
@@ -9,9 +9,9 @@ tracker.init(img, bbox)
 
 
 def drawBox():
-    x,y,w,h = int(bbox[0],bbox[1],bbox[2],bbox[3])
+    x,y,w,h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
     cv2.rectangle(img, (x,y), ((x+w), (y+h)), (255,0,255), 3,1)
-    cv2.putText(img, "Tracking", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.putText(img, "Tracking", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
 
 while True:
